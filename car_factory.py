ede6675a040780d967079aa6fd74c03afb919fc2
hugo2f <hugo.fang.25@dartmouth.edu>
2023-04-28 20:19:43 -0400

    update class model

---
+++ car_factory.py
@@ -0,0 +1,16 @@
+from car import Car
+import battery
+import engine
+
+
+def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+    return Car(engine.Capulet, battery.SpindlerBattery)
+
+# def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+    
+
+# def create_palindrome()
+
+# def create_rorschach()
+
+# def create_thovex()
